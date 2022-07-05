f5558c0b095f2b878890a42b845b69ce667ecd01
Tim Bracht <tim.bracht13100@gmail.com>
2022-07-05 22:15:35 +0200

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           COCK
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn COCK's
         </a>
       </header>
     </div>
