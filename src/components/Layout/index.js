327ec8c84b5ae6efeab7124eb0608ab5602617f5
Tim Bracht <tim.bracht13100@gmail.com>
2022-07-11 21:00:46 +0200

    sidebar and layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,23 @@
 import './index.scss'
 import Sidebar from '../Sidebar'
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
   )
 }
 export default Layout
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className='nav-bar'>
@@ -22,6 +23,24 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color={'#4d4d4e'} />
       </NavLink>
     </nav>
+    <ul type="disc">
+      <li>
+        <a
+        target="_blank"
+        rel="noreferrer"
+        href={"https://github.com"}>
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={"http://de.linkedin.com"}>
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
